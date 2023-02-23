d59bdec56c91a8feff5e524bab0806b8f620e932
Victor Brito <javibrito10@gmail.com>
2023-02-22 18:42:48 -0600

    content in country-p component moved to country component

--- src/app/pages/county-p/county-p.component.ts
+++ src/app/pages/county-p/county-p.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-county-p',
@@ -6,7 +6,6 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./county-p.component.css']
 })
 export class CountyPComponent implements OnInit {
-  @Input() country: any
 
   constructor() { }
 
