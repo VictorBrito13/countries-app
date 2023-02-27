7ccdf39202e53bf25d28db44c1d82e67dc114b21
Victor Brito <javibrito10@gmail.com>
2023-02-26 18:51:13 -0600

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllCoutriesComponent } from './pages/all-coutries/all-coutries.component';
+import { CountyPComponent } from './pages/county-p/county-p.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'all-countries'
+  },
+  {
+    path: 'all-countries',
+    component: AllCoutriesComponent
+  },
+  {
+    path: 'country/:name',
+    component: CountyPComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
