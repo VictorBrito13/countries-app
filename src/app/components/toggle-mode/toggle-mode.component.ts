6538b20d03450868861846b3e4a4e4dcfd93bd8d
Victor Brito <javibrito10@gmail.com>
2023-03-20 13:44:36 -0600

    theme switcher fixed

--- src/app/components/toggle-mode/toggle-mode.component.ts
+++ src/app/components/toggle-mode/toggle-mode.component.ts
@@ -10,18 +10,36 @@ import { faSun } from '@fortawesome/free-solid-svg-icons';
 export class ToggleModeComponent implements OnInit {
   public faMoon = faMoon
   public faSun = faSun
-  public mode!: string
+  public mode = 'Dark'
+  private documentElement = window.document.documentElement
 
   constructor() { }
 
   ngOnInit(): void {
-    if(matchMedia('(prefers-color-scheme: dark)').matches) this.mode = "Dark"
-    else this.mode = "Light"
+
   }
 
   changeMode(){
-    if(this.mode === "Dark") this.mode = "Light"
-    else this.mode = "Dark"
+    const styleElement = this.documentElement.style
+
+    function changeColor(customPorp: string, color: string): void {
+      styleElement.setProperty(customPorp, color)
+    }
+
+    if(this.mode === "Dark"){
+      this.mode = "Light"
+      changeColor('--bg', 'hsl(0, 0%, 98%)')
+      changeColor('--bg-elements', 'hsl(0, 0%, 100%)')
+      changeColor('--color-text', 'hsl(200, 15%, 8%)')
+      changeColor('--color-input', 'hsl(0, 0%, 52%)')
+    }
+    else{
+      this.mode = "Dark"
+      changeColor('--bg', 'hsl(207, 26%, 17%)')
+      changeColor('--bg-elements', 'hsl(209, 23%, 22%)')
+      changeColor('--color-text', 'hsl(0, 0%, 100%)')
+      changeColor('--color-input', 'hsl(0, 0%, 100%)')
+    }
   }
 
 }
